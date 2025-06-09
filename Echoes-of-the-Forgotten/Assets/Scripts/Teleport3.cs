c7d3a9c63697917f9ac0377206f7e119ca518d66
Vib1ing <joshivivaan19@gmail.com>
2025-06-09 14:20:03 -0400

    Teleport works PLEASE NO CONFLICT

--- Echoes-of-the-Forgotten/Assets/Scripts/Teleport3.cs
+++ Echoes-of-the-Forgotten/Assets/Scripts/Teleport3.cs
@@ -3,14 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class Teleport3 : MonoBehaviour
 {
-    public Vector3 targetPosition = new Vector3(0, 1, 0); // Change to desired spawn point
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            SpawnManager.spawnPosition = targetPosition;
-            SceneManager.LoadScene("Room 3");
-        }
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene("Room 3");
     }
 }
